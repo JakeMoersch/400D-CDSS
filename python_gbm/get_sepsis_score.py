a0facc79a66eff07b755891816c948a2eedb6f3a
Jake Moersch <Jakemoersch@gmail.com>
2021-12-04 03:58:46 -0800

    working!

--- python_gbm/get_sepsis_score.py
+++ python_gbm/get_sepsis_score.py
@@ -7,39 +7,65 @@ from pandas.core.frame import DataFrame
 import time
 
 def get_sepsis_score(data, model):
-    #start = time.time()
-    df = pd.DataFrame(data, columns = ['patient','ICULOS','HR','O2Sat','Temp','SBP','MAP','DBP','Resp','FiO2','pH','PaCO2','BUN','Calcium','Creatinine','Magnesium','Potassium','Hct','Hgb','WBC','Platelets','Age','Sex'])
-    #end = time.time()
-    #print(end-start)
+    df = pd.DataFrame(data, columns = ['ICULOS','HR','O2Sat','Temp','SBP','MAP','DBP','Resp','FiO2','pH','PaCO2','BUN','Calcium','Creatinine','Glucose', 'Magnesium','Potassium','Hct','Hgb','WBC','Platelets','Age','Sex'])
 
-    x_mean = np.array([
-23.37, 85, 97.1, 36.85, 122.73, 82.01, 62.56, 
-18.84, 0.53, 7.38, 41.16, 22.92, 7.79, 1.54, 131.05, 2.04, 4.13, 
-31.14, 10.37, 11.23, 197.32, 62.65,0])
-    x_std = np.array([
-19.2, 16.74, 2.98, 
-0.71, 23.28, 16.33, 14.05, 5.09, 0.33, 0.06, 8.78, 17.89, 2.12, 
-1.91, 46.41, 0.35, 0.59, 5.56, 1.95, 7.55, 101.61, 15.91,1])
-    #start = time.time()
+
+
+
+    x_mean = np.array([1,
+23.37, 84.99, 97.09, 36.86, 122.68, 82.00, 62.59, 
+18.86, 0.51, 7.38, 41.132, 22.72, 7.99, 1.55, 130.94, 2.03, 4.12, 
+31.07, 10.33, 11.08, 195.19, 62.65,10415.68,5341.490,11911.76,10350.12,
+1470.170,7827.760,8247.920,6080.560,35.53000,1619.560,207.6700,1619.560,7827.76])
+
+#11911.76,10350.12,
+#1470.170,7827.760,8247.920,6080.560,35.53000,1619.560,207.6700,1619.560,7827.760]
+
+    x_std = np.array([1,
+19.2, 16.74, 2.98, 0.71, 23.28, 16.33, 14.05, 5.09, 0.33, 0.06, 8.78, 17.89, 2.12, 
+1.91, 46.41, 0.35, 0.59, 5.56, 1.95, 7.55, 101.61, 15.91,
+2834.55,1660.58,2279.42,3932.20,1311.99,2877.81,1618.72,1551.45,32.9100,607.090,28.1800,607.090,2877.81])
+
+#2279.42,3932.20,1311.99,2877.81,1618.72,1551.45,32.9100,607.090,28.1800,607.090,2877.81
+
+
+
+
+
     df.interpolate(method = 'linear')
-    #end = time.time()
-    #print(end-start)
 
-    #start = time.time()
+    df['multiplyHRSBP'] = df['SBP'] * df['HR']
+    df['multiplyHRDBP'] = df['DBP'] * df['HR']
+    df['multiplyO2SatSBP'] = df['SBP'] * df['O2Sat']
+    df['multiplyMAPSBP'] = df['SBP'] * df['MAP']
+    df['multiplyHRSBP'] = df['SBP'] * df['HR']
+    df['multiplyAgeICULOS'] = df['Age'] * df['ICULOS']
+    df['multiplySBPDBP'] = df['SBP'] * df['DBP']
+    df['multiplyHRO2Sat'] = df['HR'] * df['O2Sat']
+    df['multiplyAgeO2'] = df['Age'] * df['O2Sat']
+    df['multiplyAgeSex'] = df['Age'] * df['Sex']
+    df['multiplyRespHR'] = df['Resp'] * df['HR']
+    df['addHRSBP'] = df['HR'] + df['SBP']
+    df['addHRResp'] = df['HR'] * df['Resp']
+    df['addSBPDBP'] = df['SBP'] * df['DBP']
+
+    df = df.fillna(0)
+    #df.drop(['patient'],axis=1,inplace=True)
+
+    #print(df.iloc[:,0:35])
+    
     data2 = df.to_numpy()
-    #end = time.time()
-    #print(end-start)
-    x = data2[-1, 0:23]
+    #print(data2)
+    x = data2[-1, 0:36]
+    #print(x)
+    #print(x_mean)
+    
     x_norm = np.nan_to_num((x - x_mean) / x_std)
     x_norm = np.array(x_norm)
-    x_norm = x_norm.reshape(-1,23)
-  #  x_norm = x_norm.astype(np.float64)
-    #start = time.time()
+    x_norm = x_norm.reshape(-1,36)
     score=model.predict(x_norm)
-    #end = time.time()
-    #print(end-start)
     score=min(max(score,0),1)
-    label = score > 0.0234
+    label = score > 0.0243
 
     return score, label
 
--- python_gbm/training_gbm.py
+++ python_gbm/training_gbm.py
@@ -18,19 +18,6 @@ else:
 
 print(len(CINCdat)) # should be n=198774
 
-# want to add a column of # of measurements of another column
-#HgbUnique = CINCdat['Hgb'].nunique()
-#CINCdat['HgbUnique'] = CINCdat.groupby('patient').apply(HgbUnique)
-#ICUTotalStay = CINCdat['ICULOS'].groupby('patient').count()
-#CINCdat['ICUTotalStay'] = CINCdat.groupby('patient').add(insert(0,'ICUTotalStay', ICUTotalStay))
-#CINCdat['Measurements'] = CINCdat.groupby('patient').apply(lambda group: group.apply(lambda column: column.nunique(CINCdat['Hgb'], dropna=False)))
-
-## Forward-fill missing values up to 5 sections
-
-
-
-
-##CINCdat['BP+HR'] = CINCdat['BP'] + CINCdat['HR']
 
 CINCdat = CINCdat.groupby('patient').apply(lambda column: column.interpolate(method = 'linear'))
 CINCdat = CINCdat.interpolate(method = 'linear')
@@ -38,8 +25,7 @@ CINCdat = CINCdat.interpolate(method = 'linear')
 #clipvalsSer = pd.Series(clipvals)
 
 #CINCdat = CINCdat.clip(clipvalsSer,axis=1) #replace negative data with 0
-##CINCdat.update(CINCdat.groupby('patient').ffill())
-# general statement; want to forward fill; then when we encounter a different value, we want to backfill such that i -> i+j elements each have a value equal to (v(j)-v(i) / j-i) + i
+
 CINCdat['multiplyHRSBP'] = CINCdat['SBP'] * CINCdat['HR']
 CINCdat['multiplyHRDBP'] = CINCdat['DBP'] * CINCdat['HR']
 CINCdat['multiplyO2SatSBP'] = CINCdat['SBP'] * CINCdat['O2Sat']
@@ -55,8 +41,6 @@ CINCdat['addHRSBP'] = CINCdat['HR'] + CINCdat['SBP']
 CINCdat['addHRResp'] = CINCdat['HR'] * CINCdat['Resp']
 CINCdat['addSBPDBP'] = CINCdat['SBP'] * CINCdat['DBP']
 
-
-## add a filter for multiorgan failure; 
 
 ## Get reference ranges for variables using only non-sepsis patients as 'normal'
 CINCdat_NOsepsis = CINCdat[~CINCdat.patient.isin(np.unique(CINCdat.patient[CINCdat.SepsisLabel==1]))]
@@ -75,22 +59,33 @@ for c in cols:
 
 ## Replace values still missing with the mean
 CINCdat_zScores = CINCdat_zScores.fillna(0)
+CINCdat_zScores.drop(['patient'],axis=1,inplace=True)
+CINCdat_zScoresClone = CINCdat_zScores.copy()
+CINCdat_zScores.drop(['SepsisLabel'],axis=1,inplace=True)
+#CINCdat_zScores.drop(['Sex'],axis=1,inplace=True)
 
 ## Build a linear regression model using all the training data
 ## Try a LightGBM approach
 import lightgbm as lgb
-#train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:23], label=CINCdat_zScores.SepsisLabel)
-train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:(23+14)], label=CINCdat_zScores.SepsisLabel)
+#train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:23], label=CINCdat_zScoresClone.SepsisLabel)
+train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:(23+14+1)], label=CINCdat_zScoresClone.SepsisLabel)
 param = {'objective': 'binary'}
+#print(CINCdat_zScores.iloc[:,0:(23)])
+#print(CINCdat_zScores.iloc[:,0:(23+1)])
+#print(CINCdat_zScores.iloc[:,0:(23+2)])
+print(CINCdat_zScores.iloc[:,0:(23+3)])
+
+
 bst = lgb.train(param, train_data, 10)
 
 ## Quick but not necessarily great way to find a threshold
-CINCdat_zScores = CINCdat_zScores.assign(probSepsisGBM=bst.predict(data=CINCdat_zScores.iloc[:,0:23]))
+CINCdat_zScores = CINCdat_zScores.assign(probSepsisGBM=bst.predict(data=CINCdat_zScores.iloc[:,0:(23+14+1)]))
+#CINCdat_zScores = CINCdat_zScores.assign(probSepsisGBM=bst.predict(data=CINCdat_zScores.iloc[:,0:(23)]))
 
 # Plot the AUC
 from sklearn.metrics import roc_curve, roc_auc_score
-fpr, tpr, thresholds = roc_curve(CINCdat_zScores.SepsisLabel,CINCdat_zScores.probSepsisGBM)
-print('AUC:',round(roc_auc_score(CINCdat_zScores.SepsisLabel,CINCdat_zScores.probSepsisGBM),2))
+fpr, tpr, thresholds = roc_curve(CINCdat_zScoresClone.SepsisLabel,CINCdat_zScores.probSepsisGBM)
+print('AUC:',round(roc_auc_score(CINCdat_zScoresClone.SepsisLabel,CINCdat_zScores.probSepsisGBM),2))
 
 # Save the model and get the threshold for use as a model
 bst.save_model('lightgbm.model')
