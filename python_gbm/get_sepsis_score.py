d05de7e2facb95de79c396ac9536af12b4c90585
Jake Moersch <Jakemoersch@gmail.com>
2021-12-04 02:02:07 -0800

    shit is broke

--- python_gbm/get_sepsis_score.py
+++ python_gbm/get_sepsis_score.py
@@ -4,29 +4,40 @@ import numpy as np
 import lightgbm as lgb
 import pandas as pd
 from pandas.core.frame import DataFrame
+import time
 
 def get_sepsis_score(data, model):
-
+    #start = time.time()
     df = pd.DataFrame(data, columns = ['patient','ICULOS','HR','O2Sat','Temp','SBP','MAP','DBP','Resp','FiO2','pH','PaCO2','BUN','Calcium','Creatinine','Magnesium','Potassium','Hct','Hgb','WBC','Platelets','Age','Sex'])
-    
+    #end = time.time()
+    #print(end-start)
+
     x_mean = np.array([
-23.37, 84.97, 97.09, 36.86, 122.66, 81.97, 63.35, 
-18.86, 0.49, 7.38, 41.16, 22.92, 7.79, 1.54, 131.05, 2.04, 4.13, 
+23.37, 85, 97.1, 36.85, 122.73, 82.01, 62.56, 
+18.84, 0.53, 7.38, 41.16, 22.92, 7.79, 1.54, 131.05, 2.04, 4.13, 
 31.14, 10.37, 11.23, 197.32, 62.65,0])
     x_std = np.array([
 19.2, 16.74, 2.98, 
 0.71, 23.28, 16.33, 14.05, 5.09, 0.33, 0.06, 8.78, 17.89, 2.12, 
 1.91, 46.41, 0.35, 0.59, 5.56, 1.95, 7.55, 101.61, 15.91,1])
-
+    #start = time.time()
     df.interpolate(method = 'linear')
-    
+    #end = time.time()
+    #print(end-start)
+
+    #start = time.time()
     data2 = df.to_numpy()
+    #end = time.time()
+    #print(end-start)
     x = data2[-1, 0:23]
     x_norm = np.nan_to_num((x - x_mean) / x_std)
     x_norm = np.array(x_norm)
     x_norm = x_norm.reshape(-1,23)
   #  x_norm = x_norm.astype(np.float64)
+    #start = time.time()
     score=model.predict(x_norm)
+    #end = time.time()
+    #print(end-start)
     score=min(max(score,0),1)
     label = score > 0.0234
 
--- python_gbm/training_gbm.py
+++ python_gbm/training_gbm.py
@@ -2,6 +2,7 @@
 import os, glob
 import numpy as np
 import pandas as pd
+from pandas import Series
 
 ## Load all the data so we can quickly combine it and explore it. 
 pfile = 'CinC.pickle'
@@ -17,11 +18,43 @@ else:
 
 print(len(CINCdat)) # should be n=198774
 
+# want to add a column of # of measurements of another column
+#HgbUnique = CINCdat['Hgb'].nunique()
+#CINCdat['HgbUnique'] = CINCdat.groupby('patient').apply(HgbUnique)
+#ICUTotalStay = CINCdat['ICULOS'].groupby('patient').count()
+#CINCdat['ICUTotalStay'] = CINCdat.groupby('patient').add(insert(0,'ICUTotalStay', ICUTotalStay))
+#CINCdat['Measurements'] = CINCdat.groupby('patient').apply(lambda group: group.apply(lambda column: column.nunique(CINCdat['Hgb'], dropna=False)))
+
 ## Forward-fill missing values up to 5 sections
 
-CINCdat.groupby('patient').apply(lambda group: group.interpolate(method = 'linear'))
+
+
+
+##CINCdat['BP+HR'] = CINCdat['BP'] + CINCdat['HR']
+
+CINCdat = CINCdat.groupby('patient').apply(lambda column: column.interpolate(method = 'linear'))
+CINCdat = CINCdat.interpolate(method = 'linear')
+#clipvals = np.linspace(0,0,38)
+#clipvalsSer = pd.Series(clipvals)
+
+#CINCdat = CINCdat.clip(clipvalsSer,axis=1) #replace negative data with 0
 ##CINCdat.update(CINCdat.groupby('patient').ffill())
 # general statement; want to forward fill; then when we encounter a different value, we want to backfill such that i -> i+j elements each have a value equal to (v(j)-v(i) / j-i) + i
+CINCdat['multiplyHRSBP'] = CINCdat['SBP'] * CINCdat['HR']
+CINCdat['multiplyHRDBP'] = CINCdat['DBP'] * CINCdat['HR']
+CINCdat['multiplyO2SatSBP'] = CINCdat['SBP'] * CINCdat['O2Sat']
+CINCdat['multiplyMAPSBP'] = CINCdat['SBP'] * CINCdat['MAP']
+CINCdat['multiplyHRSBP'] = CINCdat['SBP'] * CINCdat['HR']
+CINCdat['multiplyAgeICULOS'] = CINCdat['Age'] * CINCdat['ICULOS']
+CINCdat['multiplySBPDBP'] = CINCdat['SBP'] * CINCdat['DBP']
+CINCdat['multiplyHRO2Sat'] = CINCdat['HR'] * CINCdat['O2Sat']
+CINCdat['multiplyAgeO2'] = CINCdat['Age'] * CINCdat['O2Sat']
+CINCdat['multiplyAgeSex'] = CINCdat['Age'] * CINCdat['Sex']
+CINCdat['multiplyRespHR'] = CINCdat['Resp'] * CINCdat['HR']
+CINCdat['addHRSBP'] = CINCdat['HR'] + CINCdat['SBP']
+CINCdat['addHRResp'] = CINCdat['HR'] * CINCdat['Resp']
+CINCdat['addSBPDBP'] = CINCdat['SBP'] * CINCdat['DBP']
+
 
 ## add a filter for multiorgan failure; 
 
@@ -46,7 +79,8 @@ CINCdat_zScores = CINCdat_zScores.fillna(0)
 ## Build a linear regression model using all the training data
 ## Try a LightGBM approach
 import lightgbm as lgb
-train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:23], label=CINCdat_zScores.SepsisLabel)
+#train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:23], label=CINCdat_zScores.SepsisLabel)
+train_data = lgb.Dataset(data=CINCdat_zScores.iloc[:,0:(23+14)], label=CINCdat_zScores.SepsisLabel)
 param = {'objective': 'binary'}
 bst = lgb.train(param, train_data, 10)
 
@@ -61,3 +95,5 @@ print('AUC:',round(roc_auc_score(CINCdat_zScores.SepsisLabel,CINCdat_zScores.pro
 # Save the model and get the threshold for use as a model
 bst.save_model('lightgbm.model')
 print('Threshold:',round(thresholds[np.argmax(tpr - fpr)],4))
+#print('tpr',tpr)
+#print('fpr', fpr) 
